8f44e8a88c155956295f8ccd9353edebb2e2fbb4
Michael Helgesen <michaelhelgesen02@gmail.com>
2023-03-19 22:51:44 +0100

    Added forms

--- FT/__init__.py
+++ FT/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-
 #Create a Flask Instance
 app = Flask(__name__)
-
+app.secret_key = "hello"
 import FT.views
 
 if __name__ == '__main__':
--- FT/login/routes.py
+++ FT/login/routes.py
@@ -1,11 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from FT.forms import webforms
 
 login = Blueprint('login', __name__, static_folder="static", template_folder="templates")
 forgot_password = Blueprint('forgot_password', __name__)
 
-@login.route('/login')
+
+
+@login.route('/login', methods=["GET", "POST"])
 def user_login():
-    return render_template("user_login.html")
+    form = webforms.MyForm()
+    name = None
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        return render_template("user_login.html", form=form, name=name)
+    else:
+        return render_template("user_login.html", form=form)
 
 @forgot_password.route('/forgot_password')
 def user_password():
