53bc5d0e78a90166188add9f35139f42080a77aa
Michael Helgesen <michaelhelgesen02@gmail.com>
2023-06-15 10:16:06 +0200

    modal

---
+++ FT/static/js/modal.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", (event) => {
+    const myModal = new bootstrap.Modal('#exampleModal', {
+        keyboard: false,
+
+      })
+    console.log(myModal)
+  });
+
+
+/*     $(window).on('load', function() {
+        $('#myModal').modal('show');
+    }); */
